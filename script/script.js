1dd88be185b6e89c5f20eaf4216303a6e3b548ac
Tornikekhizaneishvili <khizaneishvilitornike@gmail.com>
2022-09-28 20:02:44 +0400

    update v0.01

--- script/script.js
+++ script/script.js
@@ -2,18 +2,40 @@ const featuresBtn = document.getElementById("features-btn");
 const companyBtn = document.getElementById('company-btn');
 const hamburgerBtn = document.getElementById('open');
 const hamburgerBtnClose = document.getElementById('close');
-
+const main = document.getElementById('main');
 
 featuresBtn.addEventListener('click', () => {
 
     document.getElementById("firstDropdown").classList.toggle('myDropDown-active');
+    companyBtn.addEventListener('click', () => {
 
+        document.getElementById("firstDropdown").classList.remove('myDropDown-active');
+        
+    });
+
+    main.addEventListener('click', () => {
+
+        document.getElementById("firstDropdown").classList.remove('myDropDown-active');
+        
+    });
 });
 
 
 companyBtn.addEventListener('click', () => {
 
     document.getElementById("secondDropdown").classList.toggle('myDropDown1-active');
+
+    featuresBtn.addEventListener('click', () => {
+
+        document.getElementById("secondDropdown").classList.remove('myDropDown1-active');
+    
+    });
+
+    main.addEventListener('click', () => {
+
+        document.getElementById("secondDropdown").classList.remove('myDropDown1-active');
+        
+    });
 
 });
 
@@ -32,4 +54,12 @@ hamburgerBtnClose.addEventListener('click', () => {
     document.getElementById("navbar-buttons").classList.remove('buttons-active');
     document.getElementById("navbar-menu").classList.remove('menu-active');
     hamburgerBtnClose.style.cssText = 'display: none;'
+});
+
+main.addEventListener('click', () => {
+
+    document.getElementById("navbar-buttons").classList.remove('buttons-active');
+    document.getElementById("navbar-menu").classList.remove('menu-active');
+    hamburgerBtnClose.style.cssText = 'display: none;'
+    
 });
