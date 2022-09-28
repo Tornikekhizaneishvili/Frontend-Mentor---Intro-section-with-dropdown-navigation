d81ca3cb6d5fb367b61fff0d5ae2950cedda093a
Tornikekhizaneishvili <khizaneishvilitornike@gmail.com>
2022-09-28 19:52:24 +0400

    main

---
+++ README.md
@@ -0,0 +1,3 @@
+This is my fifth projects on Frontend Mentor . 
+I made this website with Html , css and js.
+Wasn't too hard but it stills needs some adjustments.
---
+++ script/script.js
@@ -0,0 +1,35 @@
+const featuresBtn = document.getElementById("features-btn");
+const companyBtn = document.getElementById('company-btn');
+const hamburgerBtn = document.getElementById('open');
+const hamburgerBtnClose = document.getElementById('close');
+
+
+featuresBtn.addEventListener('click', () => {
+
+    document.getElementById("firstDropdown").classList.toggle('myDropDown-active');
+
+});
+
+
+companyBtn.addEventListener('click', () => {
+
+    document.getElementById("secondDropdown").classList.toggle('myDropDown1-active');
+
+});
+
+hamburgerBtn.addEventListener('click', () => {
+
+    
+    document.getElementById("navbar-buttons").classList.add('buttons-active');
+    document.getElementById("navbar-menu").classList.add('menu-active');
+    hamburgerBtnClose.style.cssText = 'display: block;'
+});
+
+
+hamburgerBtnClose.addEventListener('click', () => {
+
+    
+    document.getElementById("navbar-buttons").classList.remove('buttons-active');
+    document.getElementById("navbar-menu").classList.remove('menu-active');
+    hamburgerBtnClose.style.cssText = 'display: none;'
+});
